2302956de29c07cdc813b5e8c55e2b22a486d59d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-20 23:09:38 +0530

    marble: Enable display optimize refresh feature
    No regression observed in ~1 week of regular usage.
    
    Change-Id: Idd4521705d48aaddc746653b3d702d0b1d7f291d

--- device.mk
+++ device.mk
@@ -135,6 +135,7 @@ PRODUCT_ODM_PROPERTIES += \
     ro.surface_flinger.set_touch_timer_ms=1000 \
     vendor.display.disable_3d_adaptive_tm=0 \
     vendor.display.enable_rounded_corner=0 \
+    vendor.display.enable_optimize_refresh=1 \
     vendor.display.vds_allow_hwc=true
 
 PRODUCT_VENDOR_PROPERTIES += \
